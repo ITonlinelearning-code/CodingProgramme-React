a7855de6bb70acbea10d58eaedc3bdb8422865d3
ITonlinelearning-code <167775962+ITonlinelearning-code@users.noreply.github.com>
2024-07-25 13:54:40 +0200

    Add files via upload

---
+++ React-Practical-Activity-Example-Code-6.2.1.js
@@ -0,0 +1,6 @@
+const categoryStyles = {
+  readEmails: { backgroundColor: 'orange' },
+  sendEmails: { backgroundColor: 'yellow' },
+  webParsing: { backgroundColor: 'blue' },
+  default: { backgroundColor: 'white' }
+};
